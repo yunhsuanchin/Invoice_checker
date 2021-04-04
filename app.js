cad1ce65b24967175a9ca58cdb057aed428bc0ff
yunhsuanchin <alicechin20181@gmail.com>
2021-04-04 22:06:27 +0800

    feat: add invoice check route

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const WinningNumber = require('./models/winning_number')
+const Prize = require('./models/prize')
+require('./config/mongoose')
 const port = 3000
-const db = require('./config/mongoose')
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/', async (req, res) => {
+  try {
+    const { checkingMonth, checkingNumber } = req.body
+    const winningNumbers = await WinningNumber.find({ months: checkingMonth }).populate('prize')
+    const invoiceCheck = winningNumbers.find(item => item.number.slice(-3) === checkingNumber)
+    console.log('invoiceCheck', invoiceCheck)
+    if (invoiceCheck) {
+      return res.json({
+        status: 'success',
+        invoice: invoiceCheck.number,
+        isWinner: true,
+        amount: invoiceCheck.prize.winnings
+      })
+    } else {
+      return res.json({
+        status: 'success',
+        isWinner: false
+      })
+    }
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`)

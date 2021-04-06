690ddeb0fdf940a9324a7dba5732d5419d146d50
yunhsuanchin <alicechin20181@gmail.com>
2021-04-06 13:44:48 +0800

    remove month querying route

--- app.js
+++ app.js
@@ -41,16 +41,6 @@ app.post('/', async (req, res) => {
   }
 })
 
-app.get('/', async (req, res) => {
-  try {
-    let winningNumbers = await WinningNumber.find()
-    winningNumbers = [...new Set(winningNumbers.map(item => item.months))]
-    return res.json(winningNumbers)
-  } catch (error) {
-    console.log(error)
-  }
-})
-
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
